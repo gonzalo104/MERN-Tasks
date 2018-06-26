6921ac74cab338b3c91fe47e710abd981ceeb5e5
gonzalo104 <gonzalozavalamendoza@gmail.com>
2018-06-26 16:30:57 -0500

    first commir

---
+++ src/app/index.js
@@ -0,0 +1,12 @@
+/*
+ * @Author            : Gonzalo Zavala Mendoza
+   @email             : gonzalozavalamendoza@gmail.com
+ * @Date              : 2018-06-26 13: 03: 24
+ * @Last Modified by  : Gonzalo Zavala Mendoza
+ * @Last Modified time: 2018-06-26 13: 39: 23
+ */
+import React from 'react';
+import {render} from 'react-dom';
+import App from './app';
+
+render(<App/>, document.getElementById('app'));
